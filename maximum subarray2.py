63163cd302e94e3ba19b3bec383fc497942d07d3
vatsa2005rishika <144444479+vatsa2005rishika@users.noreply.github.com>
2026-06-27 17:11:15 +0530

    Implement maxSubArray method for maximum subarray sum

---
+++ maximum subarray2.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+       
+        max_sum = float('-inf')
+        curr_sum = 0
+        
+        for i in range(len(nums)):
+            curr_sum += nums[i]
+            max_sum = max(max_sum, curr_sum)
+ 
+            if curr_sum < 0:
+                curr_sum = 0
+        
+        return max_sum
