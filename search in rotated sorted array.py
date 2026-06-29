6341f177dc87ace56218d31ff79f9d7a57e24aeb
vatsa2005rishika <144444479+vatsa2005rishika@users.noreply.github.com>
2026-06-29 21:25:06 +0530

    Implement search in rotated sorted array

---
+++ search in rotated sorted array.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def search(self, nums, target):
+        n = len(nums)
+        l = 0
+        r = n - 1
+ 
+        while l < r:
+            m = (l + r) // 2
+ 
+            if nums[m] > nums[r]:
+                l = m + 1
+            else:
+                r = m
+ 
+        min_i = l
+ 
+        if min_i == 0:
+            l, r = 0, n - 1
+        elif target >= nums[0] and target <= nums[min_i - 1]:
+            l, r = 0, min_i - 1
+        else:
+            l, r = min_i, n - 1
+ 
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
+ 
