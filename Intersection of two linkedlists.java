7030fa77285b84f7db13cca1e78eabfa48cfe717
vatsa2005rishika <144444479+vatsa2005rishika@users.noreply.github.com>
2026-07-22 14:56:17 +0530

    Implement intersection of two linked lists

---
+++ Intersection of two linkedlists.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA==null || headB==null) return null;
+        ListNode temp1=headA;
+        ListNode temp2=headB;
+        while(temp1!=temp2){
+            temp1=(temp1!=null)?temp1.next:headB;
+            temp2=(temp2!=null)?temp2.next:headA;
+        }
+        return temp1;
+    }
+}
