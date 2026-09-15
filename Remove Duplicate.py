ad3ee28ce05faa84340413eebd306fe9e6afaa4b
vatsa2005rishika <144444479+vatsa2005rishika@users.noreply.github.com>
2026-09-15 19:57:33 +0530

    Create Remove Duplicate.py

---
+++ Remove Duplicate.py
@@ -0,0 +1,8 @@
+class solution(object):
+  def removeDuplicate(self,nums):
+    k=1
+    for i in range(1,len(nums)):
+      if nums[i]!=nums[i-1]:
+        nums[k]=nums[i]
+        k+=1
+    return k
